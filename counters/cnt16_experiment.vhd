b412c1c7f769507d8b83b4c6cb9d02134ccca645
Vasko Tchoumatchenko <vasko@eclipsesource.com>
2014-03-13 13:48:41 +0200

    Fixes clocking bug in cnt16_experiment

--- counters/cnt16_experiment.vhd
+++ counters/cnt16_experiment.vhd
@@ -17,7 +17,7 @@ end cnt16_experiment;
 
 architecture structural of cnt16_experiment is
     -- derived clock for the counter
-    signal clock : std_logic; 
+    signal clock_counter : std_logic; 
     -- derived clock for the dynamic indication
     signal clock_dyn_ind : std_logic; 
     signal cnt : unsigned(15 downto 0);
@@ -25,7 +25,7 @@ begin
 
 	u1: entity work.counter(behavioral) 
         generic map (n => 16)
-        port map( clock => clock_dyn_ind, reset => reset, q => cnt );
+        port map( clock => clock_counter, reset => reset, q => cnt );
     
     u2: entity work.dyn_ind_controler(behavioral) 
         port map( 
@@ -41,7 +41,7 @@ begin
     -- clk = clk50 / 2^24 = approx 3 Hz
     u3: entity work.clock_divider(behavioral)
     generic map( n => 24 )
-    port map( clock_in => clk50, clock_out => clock, reset => reset );
+    port map( clock_in => clk50, clock_out => clock_counter, reset => reset );
 
     -- clk_ind = clk50 / 2^16 = approx 760 Hz
     u4: entity work.clock_divider(behavioral)

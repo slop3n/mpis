58ffa8fa1860124e3438de756480a9d0cd6a1263
Vasko Tchoumatchenko <vasko@eclipsesource.com>
2014-03-25 12:13:32 +0200

    Homework 3

---
+++ homework/sequence_recognizer/fsm.vhd
@@ -0,0 +1,15 @@
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+ 
+entity fsm is
+    port ( input : in  std_logic;
+           clock : in  std_logic;
+           reset : in  std_logic;
+           output : out  std_logic);
+end fsm;
+ 
+architecture v1 of fsm is
+begin
+    -- TODO
+end;
